10dc491d4418702b40dd62924abb2d019c8018ce
Emiliano Gaucher <emilianogaucher@gmail.com>
2025-04-18 22:52:04 +1200

    Initial commit - Portfolio ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      jetbrains: ['"JetBrains Mono"', "monospace"],
+    },
+  },
+  plugins: [],
+};
